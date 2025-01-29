4f1bbbb13eebf4b41bb035224ba3cb50ded7a101
Rifat Khan <rifat6633@gmail.com>
2025-01-29 10:04:13 +0600

    feat: add /auth/login route.

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,9 +1,22 @@
+import "dotenv/config";
 import express from "express";
+import jwt from "jsonwebtoken";
+
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.post("/auth/login", (req, res) => {
+  const { username } = req.body;
+
+  const token = jwt.sign({ username }, process.env.JWT_SECRET_TOKEN as string);
+
+  res.json({ token });
 });
 
 app.listen(port, () => {
