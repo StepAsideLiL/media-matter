846a09fc551e096691616bcf5001e3d6bcf903b4
Rifat Khan <rifat6633@gmail.com>
2025-01-29 20:35:40 +0600

    fix[client]: redirect to home without any searchParams.

--- apps/client/src/components/LogoutButton.tsx
+++ apps/client/src/components/LogoutButton.tsx
@@ -1,10 +1,15 @@
 "use client";
 
 import { logoutAction } from "@/libs/actions/logout";
+import { useRouter } from "next/navigation";
 
 export default function LogoutButton() {
+  const router = useRouter();
+
   async function handleLogout() {
     await logoutAction();
+
+    router.push("/");
   }
 
   return (
