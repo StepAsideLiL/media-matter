5111b5677f2f4ef71130de5a93155a45197bfe2a
Rifat Khan <rifat6633@gmail.com>
2025-02-02 22:50:31 +0600

    feat[server]: insert user data in database.

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -2,6 +2,7 @@ import "dotenv/config";
 import express from "express";
 import jwt from "jsonwebtoken";
 import authenticateJwtToken from "./lib/authenticateJwtToken";
+import prisma from "./lib/prismadb";
 
 const app = express();
 const port = 3000;
@@ -12,10 +13,30 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.post("/auth/login", (req, res) => {
+app.post("/auth/login", async (req, res) => {
   const { username } = req.body;
-
   const token = jwt.sign({ username }, process.env.JWT_SECRET_TOKEN as string);
+
+  const userExists = await prisma.users.findUnique({
+    where: {
+      username,
+    },
+  });
+
+  if (userExists) {
+    res.json({ token: token });
+  }
+
+  const user = await prisma.users.create({
+    data: {
+      username,
+    },
+  });
+
+  if (!user) {
+    res.sendStatus(401);
+    res.json({ token: null });
+  }
 
   res.json({ token });
 });
