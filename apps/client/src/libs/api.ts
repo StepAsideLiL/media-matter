d5c1683374c50f3a37331bc0186ca45d562682a4
Rifat Khan <rifat6633@gmail.com>
2025-02-03 14:52:04 +0600

    chore: remove console log.

--- apps/client/src/libs/api.ts
+++ apps/client/src/libs/api.ts
@@ -1,7 +1,6 @@
 export default function api(route: string) {
   const baseUrl = "http://localhost:3000/";
   const formattedRoute = route.startsWith("/") ? route.slice(1) : route;
-  console.log(`${baseUrl}${formattedRoute}`);
 
   return `${baseUrl}${formattedRoute}`;
 }
